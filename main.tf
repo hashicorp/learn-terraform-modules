263a1e84a67c14599ccc0a992170db315060a64a
Robin Norwood <rnorwood@hashicorp.com>
2019-12-10 09:56:17 -0600

    Add EC2 instances to VPC

--- main.tf
+++ main.tf
@@ -19,3 +19,21 @@ module "vpc" {
 
   tags = var.vpc_tags
 }
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "my-ec2-cluster"
+  instance_count = 2
+
+  ami                    = "ami-0c5204531f799e0c6"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,8 @@ output "vpc_public_subnets" {
   description = "IDs of the VPC's public subnets"
   value       = module.vpc.public_subnets
 }
+
+output "ec2_instance_public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = module.ec2_instances.public_ip
+}

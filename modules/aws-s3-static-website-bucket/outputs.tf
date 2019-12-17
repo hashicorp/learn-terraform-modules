f963189eedaec13d8975d6b7a3bc33b9914a4f3a
Robin Norwood <rnorwood@hashicorp.com>
2019-12-17 10:08:43 -0600

    S3 static website module complete example

--- modules/aws-s3-static-website-bucket/outputs.tf
+++ modules/aws-s3-static-website-bucket/outputs.tf
@@ -1,1 +1,16 @@
 # Output variable definitions
+
+output "arn" {
+  description = "ARN of the bucket"
+  value       = aws_s3_bucket.s3_bucket.arn
+}
+
+output "name" {
+  description = "Name (id) of the bucket"
+  value       = aws_s3_bucket.s3_bucket.id
+}
+
+output "domain" {
+  description = "Domain name of the bucket"
+  value       = aws_s3_bucket.s3_bucket.website_domain
+}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,18 @@ output "ec2_instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
   value       = module.ec2_instances.public_ip
 }
+
+output "website_bucket_arn" {
+  description = "ARN of the bucket"
+  value       = module.website_s3_bucket.arn
+}
+
+output "website_bucket_name" {
+  description = "Name (id) of the bucket"
+  value       = module.website_s3_bucket.name
+}
+
+output "website_bucket_domain" {
+  description = "Domain name of the bucket"
+  value       = module.website_s3_bucket.domain
+}

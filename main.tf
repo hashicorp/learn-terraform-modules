c90354d109239b6f850d17c3bc7b61fd7b5df99a
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-11-02 10:28:42 -0800

    Modified to be init config

--- main.tf
+++ main.tf
@@ -52,14 +52,3 @@ module "ec2_instances" {
     Environment = "dev"
   }
 }
-
-module "website_s3_bucket" {
-  source = "./modules/aws-s3-static-website-bucket"
-
-  bucket_name = "robin-test-dec-17-2019"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -10,23 +10,3 @@ output "ec2_instance_public_ips" {
   value       = module.ec2_instances.public_ip
 }
 
-output "website_bucket_arn" {
-  description = "ARN of the bucket"
-  value       = module.website_s3_bucket.arn
-}
-
-output "website_bucket_name" {
-  description = "Name (id) of the bucket"
-  value       = module.website_s3_bucket.name
-}
-
-output "website_bucket_domain" {
-  description = "Domain name of the bucket"
-  value       = module.website_s3_bucket.domain
-}
-
-output "localstack_bucket_index_file" {
-  description = "Location of index file in localstack bucket"
-  value       = "${module.website_s3_bucket.name}.localhost:4566/${module.website_s3_bucket.name}/index.html"
-}
-

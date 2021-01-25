7b984d655621b399fbff52613afdc1115573b9f6
Robin Norwood <rnorwood@hashicorp.com>
2021-01-25 12:49:24 -0600

    Update for 0.14

--- modules/aws-s3-static-website-bucket/variables.tf
+++ modules/aws-s3-static-website-bucket/variables.tf
@@ -2,11 +2,11 @@
 
 variable "bucket_name" {
   description = "Name of the s3 bucket. Must be unique."
-  type = string
+  type        = string
 }
 
 variable "tags" {
   description = "Tags to set on the bucket."
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }

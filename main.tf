33c73fbf4aa0f102b2b7eff02afbf2f3a25796f6
Rita Sokolova <msokolova13@gmail.com>
2021-02-22 10:35:15 -0500

    katakoda scenario fixes

--- main.tf
+++ main.tf
@@ -50,14 +50,3 @@ module "ec2_instances" {
     Environment = "dev"
   }
 }
-
-module "website_s3_bucket" {
-  source = "./modules/aws-s3-static-website-bucket"
-
-  bucket_name = "test-123"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}

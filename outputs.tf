7c140b4be0b378fdc42775f4fc07fb72178daefe
Brian McClain <brianmmcclain@gmail.com>
2023-01-06 16:20:20 -0500

    Update vpc and ec2-instance to match tutoiral

--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,5 @@ output "vpc_public_subnets" {
 
 output "ec2_instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
-  value       = module.ec2_instances.public_ip
+  value       = module.ec2_instances[*].public_ip
 }
